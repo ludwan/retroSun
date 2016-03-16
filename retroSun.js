c494525fbda3a1255bf130ad1bfa08a22619a8ea
ludwan <ludwigsidenmark@gmail.com>
2016-03-16 15:55:32 +0100

    Added the sun

--- retroSun.js
+++ retroSun.js
@@ -1,8 +1,13 @@
 (function(){
 	var sunStage, floorStage;
 	var floorLineColor = "#C03E80";
+	var upperSunColor = "#FFF321";
+	var lowerSunColor = "#F9138F";
+	var backgroundColor= "#021921";
 	var floorX = window.innerWidth /2;
 	var floorY = -window.innerHeight / 10;
+
+	window.addEventListener('resize', resize, false);
 
 	function init(){
 		console.log("init");
@@ -13,21 +18,44 @@
 
 	function initStages(){
 		sunStage = new createjs.Stage("sunCanvas");
-		sunStage.canvas.width = window.innerWidth;
-		sunStage.canvas.height = window.innerHeight;
+		sunStage.canvas.width = window.innerWidth-10;
+		sunStage.canvas.height = window.innerHeight*0.6;
 
 		floorStage = new createjs.Stage("floorCanvas");
-		floorStage.canvas.width = window.innerWidth;
+		floorStage.canvas.width = window.innerWidth-10;
 		floorStage.canvas.height = window.innerHeight/2;
 	}
 
 	function initSun(){
 		var sun = new createjs.Shape();
-		if(window.innerWidth >= window.innerHeight){
-			sun.graphics.beginFill("yellow").drawCircle(window.innerWidth/2,window.innerHeight*0.3,window.innerHeight/4);
-		} else {
-			sun.graphics.beginFill("yellow").drawCircle(window.innerWidth/2,window.innerHeight*0.3,window.innerWidth/4);
+		console.log(window.innerHeight/4);
+		console.log(window.innerHeight/30);
+		var currPos = -window.innerHeight/40;
+		sun.graphics.beginRadialGradientFill([upperSunColor,lowerSunColor], [0, 1], 0, 0,window.innerHeight/3, window.innerHeight*0.25, window.innerHeight*0.5, window.innerHeight/2).drawCircle(0,0,window.innerHeight*0.25);
+		sun.graphics.beginFill(backgroundColor).drawRect(-window.innerHeight*0.25,window.innerHeight/4 + currPos,window.innerHeight/2, window.innerHeight/40);
+		for(var i = 0; i < 9; i++){
+			currPos = currPos -2*(window.innerHeight/(i*10+40));
+			sun.graphics.drawRect(-window.innerHeight*0.25,window.innerHeight/4 + currPos,window.innerHeight/2, window.innerHeight/(i*20+40));
+			
+			console.log(window.innerHeight/(i*20));
+
 		}
+		// currPos = currPos - 2*(window.innerHeight/40);
+		// sun.graphics.drawRect(-window.innerHeight*0.25,window.innerHeight/4 + currPos,window.innerHeight/2, window.innerHeight/(40));
+		// currPos = currPos - 2*(window.innerHeight/50);
+		// sun.graphics.drawRect(-window.innerHeight*0.25,window.innerHeight/4 + currPos,window.innerHeight/2, window.innerHeight/(50));
+
+
+		sun.x = window.innerWidth/2;
+		sun.y = window.innerHeight*0.3;
+
+		 // var blurFilter = new createjs.BlurFilter(20, 20, 1);
+	 	// 	sun.filters = [blurFilter];
+	 	// 	var bounds = blurFilter.getBounds();
+
+ 		console.log(sun);
+ 		//sun.cache(-window.innerHeight/4+bounds.x, window.innerHeight/4+bounds.y, window.innerHeight/2+bounds.width, window.innerHeight/2+bounds.height);
+
 		sunStage.addChild(sun);
 		sunStage.update();
 
@@ -54,6 +82,13 @@
 			.to({y: window.innerHeight/2}, 2000);
 		createjs.Ticker.setFPS(40);
 		createjs.Ticker.addEventListener("tick", floorStage);
+	}
+
+	function resize(){
+		sunStage.removeAllChildren();
+		sunStage.canvas.width = window.innerWidth;
+    	sunStage.canvas.height = window.innerHeight*0.6;
+    	initSun();
 	}
 
 	window.onload = function() { init() };
